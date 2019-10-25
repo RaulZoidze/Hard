c5bd77e048a319ff48b15dd987b9f24bd47bdb80
RaulZoidze <54436786+RaulZoidze@users.noreply.github.com>
2019-10-25 17:06:23 +0300

    Update hard.js

--- hard.js
+++ hard.js
@@ -1,17 +1,30 @@
-'use strict'
-let num =266219;
-num=[2, 6, 6, 2, 1, 9];
-let degree;
-// для каждого элемента массива запустить функцию,
-// промежуточный результат передавать первым аргументом далее
-let result = num.reduce(function(sum, current) {
-    num = sum * current;
-    degree = num ** 3;
+"use strict";
+let firstNum1,   
+    firstNum2;
 
-  return num;
+const question = function () { 
+    //1
+    do {
+        firstNum1 = prompt("Введите число первое");      
+      }
+      while (isNaN(firstNum1) || firstNum1 == null || firstNum1 == '');
+    //2
+    do {
+        firstNum2 = prompt("Введите число второе");
+      }
+      while (isNaN(firstNum2) || firstNum2 == null || firstNum2 == '');
+    }
+question();
 
-}, );
 
-console.log("Результат умножения: " + result);
-console.log("Результат умножения с возведением в степень: " + degree);
-console.log("Первые 2 числа :" + String (degree).slice(0,2));
+ const comparison = function () { 
+
+      if (firstNum1 > firstNum2) {
+          return 'Первое число больше второго';
+      } 
+      else if (firstNum2 === firstNum1) {
+         return 'Числа равны'
+      }
+      else { return 'Второе число больше первого'}
+    }
+alert(comparison());
